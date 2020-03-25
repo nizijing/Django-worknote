b805283d2babd7d9062311988bd535d14588ed93
zijing <nizj412@163.com>
2020-03-25 09:35:59 +0800

    make ui simple

--- pro/admin.py
+++ pro/admin.py
@@ -19,26 +19,26 @@ class EventInfoInline(admin.TabularInline):
 @admin.register(ProInfo)
 class ProInfoAdmin(admin.ModelAdmin):
 	inlines	 = (EventInfoInline,)
-	list_display	= ('pro_name', 'pro_ctime', 'pro_link', 'get_event_status')
+	list_display	= ('pro_name', 'pro_ctime', 'pro_link', 'pro_status', 'get_event_status')
 	search_fields   = ('pro_name', )
-#	ordering		= (get_event_status, )
+	ordering		= ('pro_status', )
 
 
 @admin.register(EventInfo)
 class EventInfoAdmin(admin.ModelAdmin):
 	inlines		 = (TaskInfoInline,)
-	list_display	= ('pro', 'event_name', 'event_ctime', 'event_status')
-	search_fields   = ('event_name', )
-	ordering		= ('event_status', )
-	list_editable   = ('event_status', )
+	list_display	= ('pro', 'event_name', 'event_ctime', 'event_priority', 'event_note')
+	search_fields   = ('event_name', 'event_priority')
+	ordering		= ('event_priority', 'pro', 'event_ctime')
+#	list_editable   = ('event_status', )
 
 
-@admin.register(TaskInfo)
-class TaskInfoAdmin(admin.ModelAdmin):
-	ordering	   = ('task_status', 'task_priority', 'task_mtime' )
-	list_filter	= ('task_priority', 'task_status', 'task_mtime')
-	list_display   = ('get_pro_name', 'task_id', 'task_name', 'task_status', 'task_priority', 'task_mtime', 'task_note')
-	list_editable  = ('task_status', 'task_note')
-	search_fields  = ('task_name',)
-	date_hierarchy = ('task_mtime')
+#@admin.register(TaskInfo)
+#class TaskInfoAdmin(admin.ModelAdmin):
+#	ordering	   = ('task_status', 'task_priority', 'task_mtime' )
+#	list_filter	= ('task_priority', 'task_status', 'task_mtime')
+#	list_display   = ('get_pro_name', 'task_id', 'task_name', 'task_status', 'task_priority', 'task_mtime', 'task_note')
+#	list_editable  = ('task_status', 'task_note')
+#	search_fields  = ('task_name',)
+#	date_hierarchy = ('task_mtime')
 
--- pro/models.py
+++ pro/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from datetime import datetime
 
+FINISHJOB=3
 
 # Create your models here.
 class ProInfo(models.Model):
@@ -11,7 +12,10 @@ class ProInfo(models.Model):
 	 pro_link	 = models.URLField('link')
 
 	 def get_event_status(self):
-			return all([ item.event_status for item in EventInfo.objects.filter(pro_id=self.id)])
+		 for item in EventInfo.objects.filter(pro_id=self.id):
+			 if not item.event_priority == FINISHJOB:
+				 return False
+			 return True
 	 get_event_status.boolean = True
 	 get_event_status.short_description = '事件状态'
 
@@ -24,14 +28,18 @@ class ProInfo(models.Model):
 
 
 class EventInfo(models.Model):
-	 event_status_choices = (
-		  (0, '未处理完'),
-		  (1, '已处理完'),
+	 event_priority_choices = (
+				(0, '高'),
+				(1, '中'),
+				(2, '低'),
+				(FINISHJOB, '已完成'),
+				(4, '不处理'),
 				)
-	 pro				 = models.ForeignKey(ProInfo, verbose_name = '所属项目', on_delete = models.PROTECT)
+	 pro			= models.ForeignKey(ProInfo, verbose_name = '所属项目', on_delete = models.PROTECT)
 	 event_name		= models.CharField(verbose_name = '事件名称', max_length=24)
-	 event_ctime	  = models.DateField('创建时间', auto_now_add = True)
-	 event_status	 = models.BooleanField('事件状态', default = False)
+	 event_ctime	= models.DateField('创建时间', auto_now_add = True)
+	 event_priority	= models.PositiveSmallIntegerField('优先级', choices = event_priority_choices, default = 1)
+	 event_note		= models.CharField('备注', max_length = 100, blank = True, default = '')
 
 	 def __str__(self):
 		  return self.event_name
@@ -42,12 +50,6 @@ class EventInfo(models.Model):
 
 
 class TaskInfo(models.Model):
-	 task_priority_choices = (
-				(0, '高'),
-				(1, '中'),
-				(2, '低'),
-				(3, '不处理'),
-				)
 	 task_id		  = models.ForeignKey(
 				EventInfo,
 				verbose_name = '所属事件',
@@ -57,7 +59,6 @@ class TaskInfo(models.Model):
 	 task_status = models.BooleanField(verbose_name = '任务状态', default = False)
 	 task_mtime	 = models.DateField(verbose_name = '修改时间',  auto_now=True)
 	 task_note	 = models.CharField('备注', max_length = 100, default='', blank=True)
-	 task_priority	= models.PositiveSmallIntegerField('优先级', choices = task_priority_choices, default = 1)
 
 	 def __str__(self):
 		  return self.task_name
