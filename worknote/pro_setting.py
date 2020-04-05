53323e5d0433825b7743c77259e828d5d9a9af7c
zijing <nizj412@163.com>
2020-04-05 09:58:19 +0800

    add pro_setting.py

---
+++ worknote/pro_setting.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+###################################################################
+# File Name: ldap_seting.py
+# Author: pukka
+# mail: ipaas@pukkasoft.cn
+# Created Time: Sun Apr  5 09:46:37 2020
+#=============================================================
+
+LDAP_SERVER='ldap:127.0.0.1:389'
+LDAP_ADMIN='cn=root,dc=yourdc,dc=com'
+LDAP_ADMIN_PASSWORD='yourpasswd'
+LDAP_USER_SEARCH_TPYE='ou=Users,dc=pukkasoft,dc=cn'
+LDAP_GROUP_SEARCH_TYPE='ou=Department,dc=pukkasoft,dc=cn'
+
